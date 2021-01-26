0138e977a616e0683cbbd82d5fc17fd4dc58eea5
illusion <dhruvtiwari110058@gmail.com>
2021-01-27 00:53:04 +0530

    made deploy ready

--- js/index.js
+++ js/index.js
@@ -9,16 +9,6 @@ function a(text, link) {
   );
 }
 
-function previewText(text, pathToImage) {
-  return (
-    '<span class = "hoverable green noSelect" data-image = "' +
-    pathToImage +
-    '" onmouseover="hoverData(this)" onmousedown="clickAction(this)" onmouseout="removeData(this)">' +
-    text +
-    "</span>"
-  );
-}
-
 function yellow(text) {
   return '<span class = "yellow">' + text + "</span>";
 }
